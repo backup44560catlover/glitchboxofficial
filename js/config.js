72f8267a75e7ed46c425a2b1795ec244bee110a3
backup44560catlover <backup44560@gmail.com>
2026-07-08 11:37:39 -0500

    Create js/config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+/* ==========================
+   GLITCHBOX CONFIG
+========================== */
+
+const GLITCHBOX = {
+
+    appName: "GlitchBox",
+
+    version: "1.0.0",
+
+    // Add these later
+    SUPABASE_URL: "",
+
+    SUPABASE_ANON_KEY: ""
+
+};
